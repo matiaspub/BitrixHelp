302346c46d9060308ccd9a0e62785f8e0dbae21a
Сергей <matiaspub@gmail.com>
2012-10-30 16:56:12 +0200

    api pages

--- BitrixHelp.py
+++ BitrixHelp.py
@@ -1,6 +1,20 @@
 import sublime, sublime_plugin
+import webbrowser
 
-class BitrixApiCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		self.view.insert(edit, 0, "Hello, BitrixApi!")
+settings = sublime.load_settings("BitrixHelp.sublime-settings")
+
+class BitrixHelpCommand(sublime_plugin.TextCommand):
+	def run(self, edit, it='bitrixAPI'):
+		# self.view.insert(edit, 0, "Hello, BitrixApi!")
+		pages = settings.get('pages').get(it)
+		lst = [p.get('title') for p in pages]
+		def on_done(index):
+			if index == -1:
+				return
+			url = pages[index].get('link')
+			sublime.status_message("open: "+str(url))
+			webbrowser.open_new_tab(url)
+
+
+		self.view.window().show_quick_panel(lst, on_done)
 		
